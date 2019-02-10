15909cca94ef5284848a798b7dd187dce75a252a
eastflag <eastflag@gmail.com>
2019-02-10 12:53:45 +0900

    Updates

---
+++ precache-manifest.70c683c3837028e24ce9be05370e7dc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4338c94e73989fe8b11d",
+    "url": "/react-scoreboard/static/js/runtime~main.4338c94e.js"
+  },
+  {
+    "revision": "247bb9272bf4471c40b4",
+    "url": "/react-scoreboard/static/js/main.247bb927.chunk.js"
+  },
+  {
+    "revision": "0d6597bc0859f11897c8",
+    "url": "/react-scoreboard/static/js/1.0d6597bc.chunk.js"
+  },
+  {
+    "revision": "247bb9272bf4471c40b4",
+    "url": "/react-scoreboard/static/css/main.a4ac3b5b.chunk.css"
+  },
+  {
+    "revision": "0d6597bc0859f11897c8",
+    "url": "/react-scoreboard/static/css/1.079f2fd8.chunk.css"
+  },
+  {
+    "revision": "69f64714205ec04b770e74b5755afead",
+    "url": "/react-scoreboard/index.html"
+  }
+];
